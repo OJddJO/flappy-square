7e0867671a8391ece8b525fbd575f122dad2a653
OJddJO <95376647+OJddJO@users.noreply.github.com>
2022-08-15 11:18:13 +0200

    Update

--- flappy_square.py
+++ flappy_square.py
@@ -1,4 +1,4 @@
-from kandinsky import fill_rect as fRect,draw_string as dStr
+from kandinsky import fill_rect as fRect,draw_string as dStr,get_pixel as gCol
 from ion import *
 from time import sleep
 from random import randint
